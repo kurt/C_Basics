1af5f527250d9d42576eb58e437e5e90b514ff6a
kurt <stewart.kurt2@gmail.com>
2020-04-13 17:19:23 -0700

    Added Static Counter Variable

--- Header_Functions.c
+++ Header_Functions.c
@@ -6,9 +6,13 @@ Functions Definitions
 #include "Header_Test.h"
 
 int get_amazing_result(int input){
+	static int count=0;
 	extern int global_test_variable;
 	int local_rep=10000;
 	int res;
+	
+	count++;
+	printf("Count: %d - ", count);
 	res=input*local_rep;
 	printf("Inside Function: %d\n\r",global_test_variable);
 	return res;
--- Header_Test.c
+++ Header_Test.c
@@ -11,13 +11,15 @@
 int global_test_variable=12212;
 
 int main(){
-	int result, input;
+	int i, result, input;
 	//extern int global_test_variable;
 	printf("---------------------------------\n\r");
 	printf("This is the beginning of the file\n\r");
 	printf("Starting Random Things\n\r");
 	input=12;
-	result=get_amazing_result(input);
+	for (i=0;i<10;i++){
+		result=get_amazing_result(input);
+	}
 	printf("%d is the answer\n\r",result);
 	printf("Inside MAIN %d\n\r",global_test_variable);
 }
